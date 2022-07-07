5c79a0adeede2ea44a9790c470a2375e90a1cc0d
Jose Malcher Jr <contato@josemalcher.net>
2022-07-06 22:06:16 -0300

    66 Enviando o formulário de contato via POST

--- app_super_gestao/routes/web.php
+++ app_super_gestao/routes/web.php
@@ -26,6 +26,7 @@ Route::get('/sobre-nos', function () {
 Route::get('/', 'PrincipalController@principal')->name('site.index');
 Route::get('/sobre-nos', 'SobrenosController@sobrenos')->name('site.sobrenos');
 Route::get('/contato', 'ContatoController@contato')->name('site.contato');
+Route::post('/contato', 'ContatoController@contato')->name('site.contato');
 
 /*Route::get(
     '/contato/{nome}/{categoria_id}',

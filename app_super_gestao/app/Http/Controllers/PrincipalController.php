6aaf0b4dbfdba1fb1b446980ad62f848b5c12684
Jose Malcher Jr <contato@josemalcher.net>
2022-07-12 18:06:38 -0300

    126 Ajustando o formulário de contato na rota principal

--- app_super_gestao/app/Http/Controllers/PrincipalController.php
+++ app_super_gestao/app/Http/Controllers/PrincipalController.php
@@ -8,6 +8,12 @@ class PrincipalController extends Controller
 {
     public function principal()
     {
-        return view('site.principal');
+        $motivo_contatos = [
+            '1' => 'Dúvida',
+            '2' => 'Elogio',
+            '3' => 'Reclamação'
+        ];
+
+        return view('site.principal', ['motivo_contatos' => $motivo_contatos]);
     }
 }

df52c702ef7726fcd91ada3aa05ee9eaa974f5a0
Jose Malcher Jr <contato@josemalcher.net>
2022-08-15 22:38:37 -0300

    311 Testando os endpoints de modelo

--- app_locadora_carros/app/Http/Controllers/ModeloController.php
+++ app_locadora_carros/app/Http/Controllers/ModeloController.php
@@ -93,8 +93,9 @@ class ModeloController extends Controller
      * @param  \App\Models\Modelo  $modelo
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Modelo $modelo)
+    public function update(Request $request, $id)
     {
+        $modelo = $this->modelo->find($id);
         if($modelo === null) {
             return response()->json(['erro' => 'Impossível realizar a atualização. O recurso solicitado não existe'], 404);
         }

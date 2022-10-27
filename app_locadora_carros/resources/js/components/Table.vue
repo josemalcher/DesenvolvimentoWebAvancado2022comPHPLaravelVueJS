feca58d43e2bd1f680f684cfef9d12c05e9f4449
Jose Malcher Jr <contato@josemalcher.net>
2022-10-27 15:12:05 -0300

    376 Adicionando os botões para visualização, atualização e remoção de registros

--- app_locadora_carros/resources/js/components/Table.vue
+++ app_locadora_carros/resources/js/components/Table.vue
@@ -3,34 +3,25 @@
         <table class="table table-hover">
             <thead>
             <tr>
-                <th scope="col" v-for="t, key in titulos" :key="key">{{ t.titulo }}</th>
+                <th scope="col" v-for="t, key in titulos" :key="key">{{t.titulo}}</th>
+                <th v-if="visualizar || atualizar || remover"></th>
             </tr>
             </thead>
             <tbody>
-            <!--            <tr v-for="obj in dados" :key="obj.id">
-                            &lt;!&ndash;<th scope="row">{{ m.id }}</th>
-                                <td>{{ m.nome }}</td>
-                                <td><img :src="'/storage/'+m.imagem" width="30px" height="30px"></td>&ndash;&gt;
-                            <td
-                                v-if="titulos.includes(chave)"
-                                v-for="valor, chave in obj" :key="chave"
-                            >
-                                <span v-if="chave == 'imagem'">
-                                    <img :src="'/storage/'+valor" width="30px" height="30px">
-                                </span>
-                                <span v-else>
-                                    {{ valor }}
-                                </span>
-                            </td>
-                        </tr>-->
             <tr v-for="obj, chave in dadosFiltrados" :key="chave">
                 <td v-for="valor, chaveValor in obj" :key="chaveValor">
                     <span v-if="titulos[chaveValor].tipo == 'texto'">{{valor}}</span>
-                    <span v-if="titulos[chaveValor].tipo == 'data'">{{'...'+valor}}</span>
+                    <span v-if="titulos[chaveValor].tipo == 'data'">
+                            {{ '...'+valor}}
+                        </span>
                     <span v-if="titulos[chaveValor].tipo == 'imagem'">
-                        <img :src="'/storage/'+valor" width="30px" height="30px">
-                    </span>
-
+                            <img :src="'/storage/'+valor" width="30" height="30">
+                        </span>
+                </td>
+                <td v-if="visualizar || atualizar || remover">
+                    <button v-if="visualizar" class="btn btn-outline-primary btn-sm">Visualizar</button>
+                    <button v-if="atualizar" class="btn btn-outline-primary btn-sm">Atualizar</button>
+                    <button v-if="remover" class="btn btn-outline-danger btn-sm">Remover</button>
                 </td>
             </tr>
             </tbody>
@@ -41,7 +32,7 @@
 <script>
 export default {
     name: "Table",
-    props: ['dados', 'titulos'],
+    props: ['dados', 'titulos', 'atualizar', 'visualizar', 'remover'],
     computed: {
         dadosFiltrados() {
             let campos = Object.keys(this.titulos)

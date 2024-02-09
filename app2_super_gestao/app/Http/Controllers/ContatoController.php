3cba6548306abb2012103b0583369343a53999e2
Jose Malcher Jr <contato@josemalcher.net>
2024-02-09 11:00:17 -0300

    67 Super Gestão - Enviando o formulário de contato

--- app2_super_gestao/app/Http/Controllers/ContatoController.php
+++ app2_super_gestao/app/Http/Controllers/ContatoController.php
@@ -8,6 +8,9 @@ class ContatoController extends Controller
 {
     public function contato()
     {
+
+        var_dump($_GET);
+
         return view('site.contato', ['titulo'=> 'Contato']);
     }
 }

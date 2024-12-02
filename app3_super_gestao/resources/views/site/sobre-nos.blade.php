239473021ddeccd42b891e6e478384d8ea1a5c84
Jose Malcher Jr <contato@josemalcher.net>
2024-12-02 16:05:48 -0300

    66 Blade - Realizando include de views (@include)

--- app3_super_gestao/resources/views/site/sobre-nos.blade.php
+++ app3_super_gestao/resources/views/site/sobre-nos.blade.php
@@ -1,20 +1,10 @@
 @extends('site.layouts.basico')
 
+@section('titulo', 'Sobre Nós')
+
 @section('conteudo')
-    <div class="topo">
 
-        <div class="logo">
-            <img src="img/logo.png">
-        </div>
 
-        <div class="menu">
-            <ul>
-                <li><a href="{{ route('site.index') }}">Principal</a></li>
-                <li><a href="{{ route('site.sobrenos') }}">Sobre Nós</a></li>
-                <li><a href="{{ route('site.contato') }}">Contato</a></li>
-            </ul>
-        </div>
-    </div>
 
     <div class="conteudo-pagina">
         <div class="titulo-pagina">
